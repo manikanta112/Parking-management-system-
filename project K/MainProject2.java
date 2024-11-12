25344dc2f777b63983f174ac28d0b12771287933
mani kanta <manikantachowdary112@gmail.com>
2024-11-12 13:11:59 +0530

    first commit

---
+++ project K/MainProject2.java
@@ -0,0 +1,8 @@
+public class MainProject2 {
+	public static void main(String[] args){
+	
+			userLogIn object = new userLogIn();
+			CreditCardValidation object1 = new CreditCardValidation();
+		
+	}
+}
